d0a36bf3566352e47f9991b01425a324e97814fd
Ade Adeola <adedotster@gmail.com>
2024-01-14 12:08:40 +0000

    add description to progress-view

--- screens/Stat/ProgressView.tsx
+++ screens/Stat/ProgressView.tsx
@@ -41,64 +41,76 @@ export default function ProgressView() {
   return (
     <View
       style={{
-        backgroundColor: theme.colors.primary,
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-        marginTop: 24,
         paddingBottom: 8,
       }}
     >
-      <View style={{ alignItems: "center" }}>
-        <Progress.Circle
-          size={100}
-          progress={high}
-          color={theme.colors.error}
-          unfilledColor={theme.colors.grey0}
-          borderColor={theme.colors.grey0}
-          showsText
-          thickness={9}
-          formatText={() => (
-            <Typography color={theme.colors.white} type="big">
-              {Math.floor(high * 100) + "%"}
-            </Typography>
-          )}
-        />
-        <Typography color={theme.colors.white}>High</Typography>
+      <View
+        style={{
+          backgroundColor: theme.colors.primary,
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          marginTop: 24,
+        }}
+      >
+        <View style={{ alignItems: "center" }}>
+          <Progress.Circle
+            size={100}
+            progress={high}
+            color={theme.colors.error}
+            unfilledColor={theme.colors.grey0}
+            borderColor={theme.colors.grey0}
+            showsText
+            thickness={9}
+            formatText={() => (
+              <Typography color={theme.colors.white} type="big">
+                {Math.floor(high * 100) + "%"}
+              </Typography>
+            )}
+          />
+          <Typography color={theme.colors.white}>High</Typography>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <Progress.Circle
+            size={100}
+            progress={medium}
+            color={theme.colors.warning}
+            unfilledColor={theme.colors.grey0}
+            borderColor={theme.colors.grey0}
+            showsText
+            thickness={9}
+            formatText={() => (
+              <Typography color={theme.colors.white} type="big">
+                {Math.floor(medium * 100) + "%"}
+              </Typography>
+            )}
+          />
+          <Typography color={theme.colors.white}>Medium</Typography>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <Progress.Circle
+            size={100}
+            progress={low}
+            color={theme.colors.success}
+            unfilledColor={theme.colors.grey0}
+            borderColor={theme.colors.grey0}
+            showsText
+            thickness={9}
+            formatText={() => (
+              <Typography color={theme.colors.white} type="big">
+                {Math.floor(low * 100) + "%"}
+              </Typography>
+            )}
+          />
+          <Typography color={theme.colors.white}>Low</Typography>
+        </View>
       </View>
-      <View style={{ alignItems: "center" }}>
-        <Progress.Circle
-          size={100}
-          progress={medium}
-          color={theme.colors.warning}
-          unfilledColor={theme.colors.grey0}
-          borderColor={theme.colors.grey0}
-          showsText
-          thickness={9}
-          formatText={() => (
-            <Typography color={theme.colors.white} type="big">
-              {Math.floor(medium * 100) + "%"}
-            </Typography>
-          )}
-        />
-        <Typography color={theme.colors.white}>Medium</Typography>
-      </View>
-      <View style={{ alignItems: "center" }}>
-        <Progress.Circle
-          size={100}
-          progress={low}
-          color={theme.colors.success}
-          unfilledColor={theme.colors.grey0}
-          borderColor={theme.colors.grey0}
-          showsText
-          thickness={9}
-          formatText={() => (
-            <Typography color={theme.colors.white} type="big">
-              {Math.floor(low * 100) + "%"}
-            </Typography>
-          )}
-        />
-        <Typography color={theme.colors.white}>Low</Typography>
-      </View>
+      <Typography
+        style={{ textAlign: "center" }}
+        color={theme.colors.white}
+        type="big"
+      >
+        Weekly target
+      </Typography>
     </View>
   );
 }
