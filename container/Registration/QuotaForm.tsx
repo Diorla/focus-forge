0c83e9d89b037103128cc1500eb574d8ed7e7fed
Ade Adeola <adedotster@gmail.com>
2024-01-21 22:20:32 +0000

    fix sign up logic

--- container/Registration/QuotaForm.tsx
+++ container/Registration/QuotaForm.tsx
@@ -50,7 +50,7 @@ export function QuotaForm({ name }: { name: string }) {
       email,
     });
   };
-  if (quota.useWeeklyQuota)
+  if (!quota.useWeeklyQuota)
     return (
       <View
         style={{
