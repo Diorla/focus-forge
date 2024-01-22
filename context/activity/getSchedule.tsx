6947ac60ccce5df0785384caeb6b9e83e7f1eb6b
Ade Adeola <adedotster@gmail.com>
2024-01-22 22:54:52 +0000

    sort task according to priority

--- context/activity/getSchedule.tsx
+++ context/activity/getSchedule.tsx
@@ -29,6 +29,8 @@ export type Schedule = Activity & {
  * Upcoming time (UT) => FT that is covered
  * Overflow time (OT) => FT that is not covered by AT or UT
  */
+const priority = ["high", "medium", "low", "none"];
+
 export default function getSchedule(
   activities: Activity[],
   initialTodayRemaining: number,
@@ -39,110 +41,114 @@ export default function getSchedule(
   let currentTodayRemaining = initialTodayRemaining;
   let currentUpcomingTime = initialUpcomingTime;
 
-  activities.forEach((item) => {
-    // time already done for today
-    let doneToday = 0;
-    // time already for this week, excluding today
-    let doneThisWeek = 0;
-    // today quota based on user's ideal timing
-    let todayQuota = 0;
-    // the time to spend or already spent today
-    let todayTime = 0;
-    // time left to spend today, ie today quota exceeds done today
-    let todayRemaining = 0;
-    // all extra time left for the week
-    let futureTime = 0;
-    // extra time added to today because insufficient time in the following days
-    let additionalTime = 0;
-    // time available for the following week
-    let upcomingTime = 0;
-    // No more time to do anything this week
-    let overflowTime = 0;
+  activities
+    .sort((a, b) => priority.indexOf(a.priority) - priority.indexOf(b.priority))
+    .forEach((item) => {
+      // time already done for today
+      let doneToday = 0;
+      // time already for this week, excluding today
+      let doneThisWeek = 0;
+      // today quota based on user's ideal timing
+      let todayQuota = 0;
+      // the time to spend or already spent today
+      let todayTime = 0;
+      // time left to spend today, ie today quota exceeds done today
+      let todayRemaining = 0;
+      // all extra time left for the week
+      let futureTime = 0;
+      // extra time added to today because insufficient time in the following days
+      let additionalTime = 0;
+      // time available for the following week
+      let upcomingTime = 0;
+      // No more time to do anything this week
+      let overflowTime = 0;
 
-    const doneList = Object.keys(item.done || {});
-    const timeToday = doneList.filter((datetime) => dayjs(datetime).isToday());
-    const timeThisWeek = doneList.filter(
-      (datetime) =>
-        dayjs().isSame(datetime, "week") && !dayjs(datetime).isToday()
-    );
+      const doneList = Object.keys(item.done || {});
+      const timeToday = doneList.filter((datetime) =>
+        dayjs(datetime).isToday()
+      );
+      const timeThisWeek = doneList.filter(
+        (datetime) =>
+          dayjs().isSame(datetime, "week") && !dayjs(datetime).isToday()
+      );
 
-    timeToday.forEach((date) => (doneToday += item.done[date]));
-    timeThisWeek.forEach((date) => (doneThisWeek += item.done[date]));
+      timeToday.forEach((date) => (doneToday += item.done[date]));
+      timeThisWeek.forEach((date) => (doneThisWeek += item.done[date]));
 
-    // this week remaining should be greater or equal to 0
-    const tempTWR = item.weeklyTarget - doneThisWeek;
-    const thisWeekRemaining = tempTWR > 0 ? tempTWR : 0;
+      // this week remaining should be greater or equal to 0
+      const tempTWR = item.weeklyTarget - doneThisWeek;
+      const thisWeekRemaining = tempTWR > 0 ? tempTWR : 0;
 
-    // If the time remaining this week is less than dailyLimit
-    // then we need to lower today quota to the time remaining
-    if (thisWeekRemaining > item.dailyLimit) todayQuota = item.dailyLimit;
-    else todayQuota = thisWeekRemaining;
+      // If the time remaining this week is less than dailyLimit
+      // then we need to lower today quota to the time remaining
+      if (thisWeekRemaining > item.dailyLimit) todayQuota = item.dailyLimit;
+      else todayQuota = thisWeekRemaining;
 
-    // If the time used is greater than today quota, then we need to increase
-    // today quota to match
-    if (todayQuota > doneToday) todayTime = todayQuota;
-    else todayTime = doneToday;
+      // If the time used is greater than today quota, then we need to increase
+      // today quota to match
+      if (todayQuota > doneToday) todayTime = todayQuota;
+      else todayTime = doneToday;
 
-    // the task time left to do today, it should be ge to 0
-    const tempTR = todayTime - doneToday;
+      // the task time left to do today, it should be ge to 0
+      const tempTR = todayTime - doneToday;
 
-    // Ensure that there is enough time to do task today
-    if (currentTodayRemaining > tempTR) {
-      // remove it from the cumulative today remaining
-      currentTodayRemaining -= tempTR;
-      todayRemaining = tempTR;
-    } else {
-      const extra = todayRemaining - currentTodayRemaining;
-      todayRemaining = currentTodayRemaining;
-      currentTodayRemaining = 0;
-      futureTime += extra;
-    }
-
-    // all the time till today
-    const accountedTime = doneThisWeek + doneToday + todayRemaining;
-    // Remaining time for the rest of the week
-    let unaccountedTime = item.weeklyTarget - accountedTime;
-
-    // if the time for task already exceed weekly target
-    if (unaccountedTime < 0) unaccountedTime = 0;
-    futureTime += unaccountedTime;
-
-    // Now to ensure that there is enough time in the future to complete the task
-    if (currentUpcomingTime > futureTime) {
-      //  We have enough time in the future
-      upcomingTime = futureTime;
-      currentUpcomingTime -= futureTime;
-    } else {
-      // Find the overflow time
-      const tempOverflow = futureTime - currentUpcomingTime;
-      upcomingTime = currentUpcomingTime;
-      currentUpcomingTime = 0;
-      // Assign the overflow time
-      if (currentTodayRemaining > tempOverflow) {
-        // if there is enough time today to cover overflow
-        additionalTime = tempOverflow;
+      // Ensure that there is enough time to do task today
+      if (currentTodayRemaining > tempTR) {
+        // remove it from the cumulative today remaining
+        currentTodayRemaining -= tempTR;
+        todayRemaining = tempTR;
       } else {
-        // currentTodayRemaining for user is less than overflow of the task
-        additionalTime = currentTodayRemaining;
-        // set overflow to the time not covered
-        overflowTime = tempOverflow - additionalTime;
+        const extra = todayRemaining - currentTodayRemaining;
+        todayRemaining = currentTodayRemaining;
         currentTodayRemaining = 0;
+        futureTime += extra;
       }
-    }
 
-    list.push({
-      ...item,
-      doneThisWeek,
-      doneToday,
-      thisWeekRemaining,
-      todayQuota,
-      todayTime,
-      todayRemaining,
-      futureTime,
-      additionalTime,
-      upcomingTime,
-      overflowTime,
+      // all the time till today
+      const accountedTime = doneThisWeek + doneToday + todayRemaining;
+      // Remaining time for the rest of the week
+      let unaccountedTime = item.weeklyTarget - accountedTime;
+
+      // if the time for task already exceed weekly target
+      if (unaccountedTime < 0) unaccountedTime = 0;
+      futureTime += unaccountedTime;
+
+      // Now to ensure that there is enough time in the future to complete the task
+      if (currentUpcomingTime > futureTime) {
+        //  We have enough time in the future
+        upcomingTime = futureTime;
+        currentUpcomingTime -= futureTime;
+      } else {
+        // Find the overflow time
+        const tempOverflow = futureTime - currentUpcomingTime;
+        upcomingTime = currentUpcomingTime;
+        currentUpcomingTime = 0;
+        // Assign the overflow time
+        if (currentTodayRemaining > tempOverflow) {
+          // if there is enough time today to cover overflow
+          additionalTime = tempOverflow;
+        } else {
+          // currentTodayRemaining for user is less than overflow of the task
+          additionalTime = currentTodayRemaining;
+          // set overflow to the time not covered
+          overflowTime = tempOverflow - additionalTime;
+          currentTodayRemaining = 0;
+        }
+      }
+
+      list.push({
+        ...item,
+        doneThisWeek,
+        doneToday,
+        thisWeekRemaining,
+        todayQuota,
+        todayTime,
+        todayRemaining,
+        futureTime,
+        additionalTime,
+        upcomingTime,
+        overflowTime,
+      });
     });
-  });
   return list;
 }
