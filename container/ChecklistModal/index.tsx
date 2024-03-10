a5445d5808218b96013fa506724f906c258893f4
Ade Adeola <adedotster@gmail.com>
2024-03-10 00:32:13 +0000

    fix mutliple spaces

--- container/ChecklistModal/index.tsx
+++ container/ChecklistModal/index.tsx
@@ -45,73 +45,71 @@ export default function ChecklistModal({
   };
 
   return (
-    <KeyboardWrapper>
-      <Modal visible={visible} style={{ flex: 1 }}>
-        <TopSpace />
-        <ScrollView style={{ flex: 1 }}>
-          <Typography
-            type="header"
-            style={{ textAlign: "center", paddingVertical: 4 }}
+    <Modal visible={visible} style={{ flex: 1 }}>
+      <TopSpace />
+      <ScrollView style={{ flex: 1 }}>
+        <Typography
+          type="header"
+          style={{ textAlign: "center", paddingVertical: 4 }}
+        >
+          {activity.name}
+        </Typography>
+        <Divider />
+        <View style={{ flex: 1, padding: 4, paddingHorizontal: 8 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
           >
-            {activity.name}
-          </Typography>
-          <Divider />
-          <View style={{ flex: 1, padding: 4, paddingHorizontal: 8 }}>
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <Typography type="big">Checklist</Typography>
-              <Button onPress={() => setShowAll(!showAll)}>
-                {showAll ? "Hide completed" : "Show all"}
-              </Button>
-            </View>
-            {taskList
-              .filter((item) => {
-                if (showAll) return true;
-                return !item.checked;
-              })
-              .sort((a, b) => a.created - b.created)
-              .map((item) => {
-                return (
-                  <View
-                    key={item.created}
-                    style={{
-                      flexDirection: "row",
-                      alignItems: "center",
-                      justifyContent: "space-between",
-                      flex: 1,
-                    }}
-                  >
-                    <View style={{ flex: 1 }}>
-                      <CheckBox
-                        checked={!!item.checked}
-                        onPress={() =>
-                          item.checked
-                            ? uncheckTask(item.id)
-                            : checkTask(item.id)
-                        }
-                        iconType="material-community"
-                        checkedIcon="checkbox-marked"
-                        uncheckedIcon="checkbox-blank-outline"
-                        title={item.title}
-                      />
-                    </View>
-                    <MaterialIcons
-                      name="delete"
-                      size={28}
-                      color="black"
-                      onPress={() => {
-                        deleteTask(item.id);
-                        setTaskList(taskList.filter((i) => i.id !== item.id));
-                      }}
+            <Typography type="big">Checklist</Typography>
+            <Button onPress={() => setShowAll(!showAll)}>
+              {showAll ? "Hide completed" : "Show all"}
+            </Button>
+          </View>
+          {taskList
+            .filter((item) => {
+              if (showAll) return true;
+              return !item.checked;
+            })
+            .sort((a, b) => a.created - b.created)
+            .map((item) => {
+              return (
+                <View
+                  key={item.created}
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    flex: 1,
+                  }}
+                >
+                  <View style={{ flex: 1 }}>
+                    <CheckBox
+                      checked={!!item.checked}
+                      onPress={() =>
+                        item.checked ? uncheckTask(item.id) : checkTask(item.id)
+                      }
+                      iconType="material-community"
+                      checkedIcon="checkbox-marked"
+                      uncheckedIcon="checkbox-blank-outline"
+                      title={item.title}
                     />
                   </View>
-                );
-              })}
+                  <MaterialIcons
+                    name="delete"
+                    size={28}
+                    color="black"
+                    onPress={() => {
+                      deleteTask(item.id);
+                      setTaskList(taskList.filter((i) => i.id !== item.id));
+                    }}
+                  />
+                </View>
+              );
+            })}
+          <KeyboardWrapper>
             {showAddNewTask ? (
               <View>
                 <Input
@@ -152,19 +150,19 @@ export default function ChecklistModal({
                 </View>
               </View>
             ) : null}
-            {showAddNewTask ? null : (
-              <View>
-                <Button onPress={() => setShowAddNewTask(!showAddNewTask)}>
-                  New task
-                </Button>
-              </View>
-            )}
-          </View>
-        </ScrollView>
-        <Button onPress={closeModal} style={{ marginBottom: 25 }}>
-          Close
-        </Button>
-      </Modal>
-    </KeyboardWrapper>
+          </KeyboardWrapper>
+          {showAddNewTask ? null : (
+            <View>
+              <Button onPress={() => setShowAddNewTask(!showAddNewTask)}>
+                New task
+              </Button>
+            </View>
+          )}
+        </View>
+      </ScrollView>
+      <Button onPress={closeModal} style={{ marginBottom: 25 }}>
+        Close
+      </Button>
+    </Modal>
   );
 }
