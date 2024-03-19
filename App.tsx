decb567d75462c6351fd63f400c5c38b2c3a91a7
Ade Adeola <adedotster@gmail.com>
2024-03-19 12:19:34 +0000

    initial boundary wrapper

--- App.tsx
+++ App.tsx
@@ -5,6 +5,10 @@ import { StatusBar } from "expo-status-bar";
 import UserProvider from "./context/user";
 import Root from "./Root";
 import "expo-dev-client";
+import ErrorBoundary from "react-native-error-boundary";
+import { logError } from "./services/database";
+import { Typography } from "./components";
+import { Button, View } from "react-native";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -14,13 +18,26 @@ Notifications.setNotificationHandler({
   }),
 });
 
+const CustomFallback = (props: { error: Error }) => (
+  <View>
+    <Typography>Something happened!</Typography>
+    <Typography>{props.error.toString()}</Typography>
+    <Button title={"Try again"} />
+  </View>
+);
+
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <UserProvider>
-        <Root />
-        <StatusBar style="auto" />
-      </UserProvider>
-    </ThemeProvider>
+    <ErrorBoundary
+      onError={(error) => logError("error boundary", "uncaught error", error)}
+      FallbackComponent={CustomFallback}
+    >
+      <ThemeProvider theme={theme}>
+        <UserProvider>
+          <Root />
+          <StatusBar style="auto" />
+        </UserProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
