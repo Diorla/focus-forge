513322b9fa7bf5bf827c8da9d5c94116d1585768
Ade Adeola <adedotster@gmail.com>
2024-06-15 15:05:00 +0100

    add error boundary

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,24 +9,55 @@ import UserProvider from "@/context/user";
 import DataProvider from "@/context/data";
 import { ToastProvider } from "react-native-toast-notifications";
 import ScheduleProvider from "@/context/schedule";
-import { useColorScheme } from "react-native";
+import { GestureResponderEvent, useColorScheme } from "react-native";
+import ErrorBoundary from "react-native-error-boundary";
+import { logError } from "@/services/database";
+import { ThemedView } from "@/components/ThemedView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedButton } from "@/components/ThemedButton";
+
+const CustomFallback = (props: {
+  error: Error;
+  resetError: (event: GestureResponderEvent) => void;
+}) => {
+  return (
+    <ThemedView
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <ThemedText>Something went wrong</ThemedText>
+      <ThemedText>{props.error.toString()}</ThemedText>
+      <ThemedButton
+        title="Try again"
+        onPress={(event) => {
+          logError("custom fall back", "page load", props.error);
+          props.resetError(event);
+        }}
+      />
+    </ThemedView>
+  );
+};
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ToastProvider>
-      <UserProvider>
-        <DataProvider>
-          <ScheduleProvider>
-            <ThemeProvider
-              value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-            >
-              <Slot />
-            </ThemeProvider>
-          </ScheduleProvider>
-        </DataProvider>
-      </UserProvider>
-    </ToastProvider>
+    <UserProvider>
+      <ErrorBoundary
+        onError={(error) => logError("error boundary", "uncaught error", error)}
+        FallbackComponent={CustomFallback}
+      >
+        <ToastProvider>
+          <DataProvider>
+            <ScheduleProvider>
+              <ThemeProvider
+                value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+              >
+                <Slot />
+              </ThemeProvider>
+            </ScheduleProvider>
+          </DataProvider>
+        </ToastProvider>
+      </ErrorBoundary>
+    </UserProvider>
   );
 }
