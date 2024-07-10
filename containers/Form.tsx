34f2756f5227e25befbe1cc38d11668849d90170
Ade Adeola <adedotster@gmail.com>
2024-07-10 14:11:03 +0100

    forgot password updated

--- containers/Form.tsx
+++ containers/Form.tsx
@@ -10,18 +10,9 @@ import signIn from "@/services/auth/signIn";
 import useUser from "@/context/user/useUser";
 import saveUserCred from "@/services/database/saveUserCred";
 import { logError } from "@/services/database";
-
-const ErrorReader = (message: string) => {
-  if (message.includes("invalid-credential"))
-    return "Email and password does not match";
-  if (message.includes("email-already-in-use")) return "Email already in use";
-  if (message.includes("weak-password")) return "Password is too weak";
-  if (message.includes("user-not-found")) return "Email does not exist";
-  if (message.includes("invalid-email")) return "Email is invalid";
-  if (message.includes("too-many-requests"))
-    return "Too many failed sign in, please reset password";
-  return message;
-};
+import resetPassword from "@/services/auth/resetPassword";
+import { ThemedText } from "@/components/ThemedText";
+import ErrorReader from "./ErrorReader";
 
 const baseForm = {
   email: "",
@@ -37,6 +28,37 @@ export default function FormScreen() {
 
   const [isNew, setIsNew] = useState(false);
   const theme = useThemeColor();
+  const [isResetPassword, setIsResetPassword] = useState(false);
+  const [resetConfirmed, setResetConfirmed] = useState(false);
+
+  const forgotPassword = () => {
+    const { email } = form;
+    setLoading(true);
+    if (!email) {
+      setFormError({
+        ...baseForm,
+        email: "Email is required",
+      });
+
+      setLoading(false);
+      return;
+    }
+
+    resetPassword(email)
+      .then(() => {
+        setResetConfirmed(true);
+      })
+      .catch((err) => {
+        setFormError({
+          ...baseForm,
+          email: ErrorReader(err.message),
+        });
+        setLoading(false);
+        logError(email, "resetting password", err);
+      });
+
+    setLoading(false);
+  };
 
   const submit = () => {
     const { email, password } = form;
@@ -95,9 +117,91 @@ export default function FormScreen() {
     if (user.id && user.id !== "user") goBack();
   }, [user]);
 
+  if (resetConfirmed)
+    return (
+      <ParallaxScrollView name="enter">
+        <ThemedText
+          type="title"
+          style={{ textAlign: "center", marginVertical: 20 }}
+        >
+          Reset password
+        </ThemedText>
+        <ThemedView style={{ padding: 8, alignItems: "center" }}>
+          <ThemedText style={{ textAlign: "center", marginBottom: 20 }}>
+            Password reset completed, please check your email for further
+            instruction and then come back
+          </ThemedText>
+          <ThemedButton
+            title="Go back"
+            onPress={() => {
+              setIsResetPassword(false);
+              setResetConfirmed(false);
+              setIsNew(false);
+            }}
+            color={theme.primary}
+          />
+        </ThemedView>
+      </ParallaxScrollView>
+    );
+  if (isResetPassword)
+    return (
+      <ParallaxScrollView name="enter">
+        <ThemedText
+          type="title"
+          style={{ textAlign: "center", marginVertical: 20 }}
+        >
+          Reset password
+        </ThemedText>
+        <ThemedView>
+          <ThemedInput
+            label="Email"
+            value={form.email}
+            errorMessage={formError.email}
+          />
+          <ThemedView
+            style={{
+              justifyContent: "space-between",
+              flexDirection: "row",
+              paddingHorizontal: 8,
+            }}
+          >
+            <ThemedButton
+              title="Login"
+              onPress={() => {
+                setIsNew(false);
+                setIsResetPassword(false);
+              }}
+              color={theme.primary}
+            />
+            <ThemedButton
+              title="Create Account"
+              onPress={() => {
+                setIsNew(true);
+                setIsResetPassword(false);
+              }}
+              color={theme.primary}
+            />
+          </ThemedView>
+          <ThemedView style={{ alignItems: "center", marginVertical: 36 }}>
+            <ThemedButton
+              title="Submit"
+              outlined
+              onPress={forgotPassword}
+              loading={loading}
+            />
+          </ThemedView>
+        </ThemedView>
+      </ParallaxScrollView>
+    );
   return (
     <ParallaxScrollView name="enter">
-      <ThemedView style={{ marginTop: 40 }}>
+      <ThemedText
+        type="title"
+        style={{ textAlign: "center", marginVertical: 20 }}
+      >
+        {isNew ? "Create Account" : "Login"}
+      </ThemedText>
+      <ThemedView>
         <ThemedInput
           label="Email"
           value={form.email}
@@ -145,11 +249,11 @@ export default function FormScreen() {
       >
         <ThemedButton
           title="Forgot password"
-          onPress={() => setIsNew(!isNew)}
+          onPress={() => setIsResetPassword(true)}
           color={theme.grey0}
         />
         <ThemedButton
-          title={isNew ? "Login" : "Create Account"}
+          title={isNew ? "Already a member?" : "New member?"}
           onPress={() => setIsNew(!isNew)}
           color={theme.primary}
         />
