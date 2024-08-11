b5d3f4588faad2a3988321d2c95fee7a180987df
Ade Adeola <adedotster@gmail.com>
2024-08-11 16:26:27 +0100

    improve sorting

--- containers/Home/TodoView.tsx
+++ containers/Home/TodoView.tsx
@@ -2,26 +2,8 @@ import { ScrollView } from "react-native";
 import useSchedule from "../../context/schedule/useSchedule";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import Checklist from "@/context/schedule/Checklist";
-import dayjs from "dayjs";
 import ProjectCard from "@/components/ProjectCard";
-
-const daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-const getChecksValue = (item: Checklist) => {
-  if (item.occurrenceType === "daily") {
-    const hoursRemaining = 24 - dayjs().hour();
-    return hoursRemaining / item.remaining;
-  }
-  if (item.occurrenceType === "weekly") {
-    const daysLeft = 7 - dayjs().day();
-    const value = (daysLeft * 24) / item.remaining;
-    return value - dayjs().hour();
-  }
-  const currentMonth = dayjs().month();
-  const daysLeft = daysInMonth[currentMonth] - dayjs().date();
-  const value = (daysLeft * 24) / item.remaining;
-  return value - dayjs().hour();
-};
+import getHoursRemaining from "./getHoursRemaining";
 
 export default function TodoView() {
   const { checklist } = useSchedule();
@@ -38,7 +20,7 @@ export default function TodoView() {
           .filter((item) => !item.archived)
           .sort((a, b) => a.doneTimes - b.doneTimes)
           .sort((a, b) => (a.occurrenceStart || 0) - (b.occurrenceStart || 0))
-          .sort((a, b) => getChecksValue(a) - getChecksValue(b))
+          .sort((a, b) => getHoursRemaining(a) - getHoursRemaining(b))
           .map((item) => (
             <ProjectCard item={item} key={item.id} type="todo" />
           ))}
