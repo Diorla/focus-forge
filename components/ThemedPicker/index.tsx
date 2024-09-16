ffd0763c59bea702b252ae3232e361527529892d
Ade Adeola <adedotster@gmail.com>
2024-09-16 08:40:31 +0100

    use default

--- components/ThemedPicker/index.tsx
+++ components/ThemedPicker/index.tsx
@@ -1,10 +1,5 @@
 import { Picker as RNPicker } from "@react-native-picker/picker";
-import { Platform } from "react-native";
-import { useState } from "react";
-import { ThemedText } from "../ThemedText";
-import { ThemedButton } from "../ThemedButton";
 import { ThemedView } from "../ThemedView";
-import ThemedModal from "../ThemedModal";
 import useUser from "@/context/user/useUser";
 import ThemedPickerProps from "./ThemedPickerProps";
 import Label from "../Label";
@@ -17,75 +12,19 @@ export default function ThemedPicker({
   labelStyle = {},
 }: ThemedPickerProps) {
   const { theme } = useUser();
-  const [showPicker, setShowPicker] = useState(false);
-  if (Platform.OS === "android")
-    return (
-      <ThemedView>
-        <Label label={label || ""} style={labelStyle} />
-        <RNPicker
-          itemStyle={{ color: theme.text }}
-          selectedValue={value}
-          onValueChange={onValueChange}
-          style={{ color: theme.grey0 }}
-        >
-          {list.map((item, idx) => (
-            <RNPicker.Item key={idx} {...item} style={{ fontSize: 16 }} />
-          ))}
-        </RNPicker>
-      </ThemedView>
-    );
-
-  const labelValue =
-    list.find((item) => item.value === value)?.label || "No label";
   return (
     <ThemedView>
       <Label label={label || ""} style={labelStyle} />
-      <ThemedView
-        style={{
-          alignItems: "flex-start",
-          marginLeft: 36,
-          marginBottom: 12,
-          marginTop: 4,
-        }}
+      <RNPicker
+        itemStyle={{ color: theme.text }}
+        selectedValue={value}
+        onValueChange={onValueChange}
+        style={{ color: theme.grey0, backgroundColor: theme.background }}
       >
-        <ThemedButton
-          onPress={() => setShowPicker(!showPicker)}
-          outlined
-          title={labelValue}
-        />
-      </ThemedView>
-      <ThemedModal visible={showPicker}>
-        <ThemedView
-          style={{
-            justifyContent: "center",
-            flex: 1,
-          }}
-        >
-          <ThemedText
-            style={{
-              marginLeft: 8,
-              color: theme.grey2,
-              fontWeight: "bold",
-            }}
-          >
-            {label}
-          </ThemedText>
-          <RNPicker selectedValue={value} onValueChange={onValueChange}>
-            {list.map((item, idx) => (
-              <RNPicker.Item key={idx} {...item} style={{ fontSize: 16 }} />
-            ))}
-          </RNPicker>
-          <ThemedView
-            style={{ flexDirection: "row", justifyContent: "center" }}
-          >
-            <ThemedButton
-              onPress={() => setShowPicker(!showPicker)}
-              title="Close"
-              outlined
-            />
-          </ThemedView>
-        </ThemedView>
-      </ThemedModal>
+        {list.map((item, idx) => (
+          <RNPicker.Item key={idx} {...item} style={{ fontSize: 16 }} />
+        ))}
+      </RNPicker>
     </ThemedView>
   );
 }
