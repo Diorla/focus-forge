e40cd9559498e6d88384a14addb5ef51da7f17f8
Ade Adeola <adedotster@gmail.com>
2024-10-02 09:10:34 +0100

    filter due and not due

--- containers/Home/TodoView.tsx
+++ containers/Home/TodoView.tsx
@@ -5,10 +5,29 @@ import { ThemedView } from "@/components/ThemedView";
 import ActivityCard from "@/components/ActivityCard";
 import getHoursRemaining from "./getHoursRemaining";
 
+const isTimePassed = (time: number) => time < new Date().getHours();
+const sortByTimePassed = (prev = 0, next = 0) => {
+  if (isTimePassed(prev) && isTimePassed(next)) return 0;
+  if (isTimePassed(prev)) return -1;
+  if (isTimePassed(next)) return 1;
+  return prev - next;
+};
+
 export default function TodoView() {
   const { checklist } = useSchedule();
 
   const todo = checklist.filter((item) => item.remaining);
+
+  console.log(
+    todo
+      .filter((item) => !item.archived)
+      .map((item) => {
+        return {
+          name: item.name,
+          start: item.occurrenceStart,
+        };
+      })
+  );
 
   return (
     <ThemedView style={{ marginVertical: 8, paddingVertical: 8 }}>
@@ -19,8 +38,10 @@ export default function TodoView() {
         {todo
           .filter((item) => !item.archived)
           .sort((a, b) => a.doneTimes - b.doneTimes)
-          .sort((a, b) => (a.occurrenceStart || 0) - (b.occurrenceStart || 0))
           .sort((a, b) => getHoursRemaining(a) - getHoursRemaining(b))
+          .sort((a, b) =>
+            sortByTimePassed(a.occurrenceStart, b.occurrenceStart)
+          )
           .map((item) => (
             <ActivityCard item={item} key={item.id} type="todo" />
           ))}
