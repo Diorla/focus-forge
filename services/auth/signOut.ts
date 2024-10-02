982b8da621cc365ab654c2d2b2a6c4241c51ebf2
Ade Adeola <adedotster@gmail.com>
2024-10-02 08:26:54 +0100

    remove login details

--- services/auth/signOut.ts
+++ services/auth/signOut.ts
@@ -1,12 +1,13 @@
 import { getAuth, signOut as signOutFB } from "firebase/auth";
-import app from "../../constants/firebaseConfig";
+import app from "@/constants/firebaseConfig";
+import removeUserCred from "../database/removeUserCred";
 
 const auth = getAuth(app);
 
 export default async function signOut() {
   return signOutFB(auth)
     .then(() => {
-      console.log("signed out");
+      removeUserCred();
     })
     .catch((err) => console.log(err));
 }
