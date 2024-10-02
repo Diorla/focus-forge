63a3e8c812ee5f42398808691482765178b3bd53
Ade Adeola <adedotster@gmail.com>
2024-10-02 10:55:45 +0100

    improve dating

--- containers/Activity/History/DailyList.tsx
+++ containers/Activity/History/DailyList.tsx
@@ -4,6 +4,7 @@ import HistoryItem from "./HistoryItem";
 import { ThemedText } from "@/components/ThemedText";
 import generateHistoryHeader from "./generateHistoryHeader";
 import dayjs from "dayjs";
+import CompletedInfo from "./CompletedInfo";
 
 export default function DailyList({
   list,
@@ -16,9 +17,20 @@ export default function DailyList({
   activityId: string;
   date: string;
 }) {
+  const totalTime = list.reduce((acc, next) => acc + next.length, 0);
   return (
     <ThemedView>
-      <ThemedText>{generateHistoryHeader(date)}</ThemedText>
+      <ThemedView
+        style={{
+          justifyContent: "space-between",
+          flexDirection: "row",
+          marginTop: 10,
+          marginRight: 10,
+        }}
+      >
+        <ThemedText>{generateHistoryHeader(date)}</ThemedText>
+        <CompletedInfo isOccurrence={isOccurrence} value={totalTime} />
+      </ThemedView>
       <ThemedView>
         {list
           .sort((a, b) => dayjs(b.datetime).diff(a.datetime))
--- containers/Activity/History/generateHistoryHeader.tsx
+++ containers/Activity/History/generateHistoryHeader.tsx
@@ -4,5 +4,8 @@ import dayjs from "dayjs";
 export default function generateHistoryHeader(date: string) {
   if (dayjs(date).isToday()) return "Today";
   if (dayjs().diff(date, "day") === 1) return "Yesterday";
+  if (dayjs().diff(date, "week") === 0) return dayjs(date).format("dddd");
+  if (dayjs().diff(date, "day") === 7)
+    return "Last " + dayjs(date).format("dddd");
   return format(date, "date");
 }
